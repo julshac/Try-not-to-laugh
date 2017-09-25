b4e7476447163ca2ac6dc52bc9fd8416f44f4101
Forcause <hrishtakyan@inbox.ru>
2017-09-25 19:01:24 +0300

    Add ReadKey

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            int b = 101;
+            if (a < b)
+            {
+                a = 2 * a;
+            }
+        else 
+            {
+                b = a;
+            }
+            System.Console.WriteLine("Значение а = {0}, Значение b = {1}", a, b);
+            Console.ReadKey();
+        }
+    }
+}
